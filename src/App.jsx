42898b22373ad498a910f572a8437b2f4d601b26
anthrofax <afridhoikhsan@gmail.com>
2024-01-05 08:09:49 +0700

    Handling Errors With Error Elements

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import Order from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
+import Error from "./ui/Error.jsx";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error/>,
     children: [
       {
         path: "/",
@@ -17,7 +19,8 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
-        loader: menuLoader
+        loader: menuLoader,
+        errorElement: <Error/>
       },
       {
         path: "/cart",
