7985c25954b245c08c0242a09368fde81b552bb3
anthrofax <afridhoikhsan@gmail.com>
2024-01-05 07:52:50 +0700

    Connecting route with loader function to fetch data on every render

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu.jsx";
+import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import Order from "./features/order/Order.jsx";
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader
       },
       {
         path: "/cart",
